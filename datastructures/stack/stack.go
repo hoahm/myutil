d4528cc3b756859f149e271c26d576316528ca4a
Hoang Hoa <nobi.hoa@gmail.com>
2017-11-30 10:45:19 +0700

    Implement stack data structure

---
+++ datastructures/stack/stack.go
@@ -0,0 +1,65 @@
+// MIT License
+// 
+// Copyright (c) 2017 Nobi Hoang <nobi.hoa@gmail.com>
+// 
+
+package stack
+
+type node struct {
+	value interface{}
+	prev *node	
+}
+
+// Stack is a last in - first out (LIFO) data structure
+type Stack struct {
+	top *node
+	length int
+}
+
+// New initialize an empty stack
+func New() *Stack {
+	return &Stack{nil, 0}
+}
+
+// IsEmpty check if stack is empty or not
+func (s *Stack) IsEmpty() bool {
+	if s.length == 0 {
+		return true
+	}
+	return false
+}
+
+// Len returns the number elements of stack
+func (s *Stack) Len() int { 
+	return s.length 
+}
+
+// Size is the same with Len()
+func (s *Stack) Size() int {
+	return s.length
+}
+
+// Top get the value of the peak element of stack
+func (s *Stack) Top() interface{} {
+	if s.length == 0 {
+		return nil
+	}
+	return s.top.value
+}
+
+// Pop get and remove the value of the peak element of stack
+func (s *Stack) Pop() interface{} {
+	if s.length == 0 {
+		return nil
+	}
+	top := s.top
+	s.top = top.prev
+	return top.value
+}
+
+// Push add new element to the peak of stack
+func (s *Stack) Push(value interface{}) {
+	n := &node{value, s.top}
+	s.top = n
+	s.length++
+}
---
+++ datastructures/stack/stack_test.go
@@ -0,0 +1,77 @@
+// MIT License
+// 
+// Copyright (c) 2017 Nobi Hoang <nobi.hoa@gmail.com>
+// 
+
+package stack_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/hoahm/myutil/datastructures/stack"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+)
+
+func TestStack(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Stack Suit")
+}
+
+var _ = Describe("Stack", func() {
+	Context("empty", func() {
+		var s = New()
+
+		It("should be empty", func() {
+			fmt.Print("Len = ", s.Len())
+			fmt.Print("Top = ", s.Top())
+			Expect(s.IsEmpty()).To(BeTrue())
+		})
+
+		It("length is 0", func() {
+			Expect(s.Len()).To(Equal(0))
+		})
+
+		It("peak element is nil", func() {
+			Expect(s.Top()).To(BeNil())
+		})
+
+		It("pop return nil", func() {
+			Expect(s.Pop()).To(BeNil())
+		})
+	})
+
+	Context("not empty", func() {
+		var s Stack
+
+		BeforeSuite(func() {
+			s.Push(1)
+		})
+
+		It("should not be empty", func() {
+			Expect(s.IsEmpty()).To(BeFalse())
+		})
+
+		It("length is 1", func() {
+			Expect(s.Len()).To(Equal(1))
+		})
+
+		It("push increase len", func() {
+			s.Push(2)
+			Expect(s.Len()).To(Equal(2))
+		})
+
+		It("peak element is not nil", func() {
+			Expect(s.Top()).NotTo(BeNil())
+		})
+
+		It("peak element has value 2", func() {
+			Expect(s.Top()).To(Equal(2))
+		})
+
+		It("pop return 2", func() {
+			Expect(s.Pop()).To(Equal(2))
+		})
+	})
+})
